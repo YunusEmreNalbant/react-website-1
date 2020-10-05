503be04c8b28595c6dbe4fef2ab1e6797e24020a
YunusEmreNalbant <nalbantyunusemre@hotmail.com>
2020-10-05 13:44:12 +0300

    Creating our Pages

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -10,7 +10,7 @@ function HeroSection() {
             <p>What are you waiting for?</p>
             <div className="hero-btns">
                 <Button className="btns" buttonStyle='btn--outline' buttonSize="btn--large">GET STARTED</Button>
-                <Button className="btns" buttonStyle='btn--outline' buttonSize="btn--large">WATCH TRAILER <i class="far-fa-play-circle"/></Button>
+                <Button className="btns" buttonStyle='btn--primary' buttonSize="btn--large">WATCH TRAILER <i class="far-fa-play-circle"/></Button>
             </div>
 
         </div>
