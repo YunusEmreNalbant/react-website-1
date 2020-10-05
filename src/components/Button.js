3808a860751f74f9617797045b1f1fc8f2f5aa80
YunusEmreNalbant <nalbantyunusemre@hotmail.com>
2020-10-05 17:30:55 +0300

    Creating the Card Components and Other Pages

--- src/components/Button.js
+++ src/components/Button.js
@@ -22,6 +22,7 @@ export const Button = ({
                     onClick={onClick}
                     type={type}
             >
+                
                 {children}
             </button>
 
---
+++ src/components/pages/Products.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import '../../App.css';
+
+
+function Products() {
+    return (
+        <div>
+            products   
+        </div>
+    )
+}
+
+export default Products
